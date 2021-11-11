447bc2cdd4261eb634819552a12b351ee8222dbf
caohuijun2018 <1160364324@qq.com>
2021-11-11 15:50:19 +0800

    feature():创建Client，构建请求类

---
+++ Client.js
@@ -0,0 +1,52 @@
+const { request } = require("http");
+const net = require("net");
+
+
+class Request {
+  constructor(options) {
+    this.method = options.method || "Get";
+    this.host = options.host;
+    this.post = options.post || "80";
+    this.path = options.path || "/";
+    this.body = options.body || {};
+    this.headers = options.headers || {};
+    if (!this.headers["Content-Type"]) {
+      this.headers["Content-Type"] = "application/x-www-form-urlencoded";
+    }
+    if (this.headers["Content-Type"] === "application/json") {
+      this.bodyText = JSON.stringify(this.body);
+    } else if (
+      (this.headers["Content-Type"] = "application/x-www-form-urlencoded")
+    ) {
+      this.bodyText = Object.keys(this.body)
+        .map((key) => `${key} = ${encodeURIComponent(this.body[key])}`)
+        .join("&");
+    }
+
+    this.headers["Content-Length"] = this.bodyText.length;
+  }
+
+  send() {
+      return new Promise((request,response) => {
+          ///
+      })
+  }
+}
+
+void async function () {
+  let request = new Request({
+    method: "Post",
+    host: "127.0.0.1",
+    post: "8088",
+    path: "/",
+    headers: {
+      ["X-Foo2"]: "customed",
+    },
+    body: {
+      name: "Lisa",
+    },
+  });
+
+  let response = await request.send();
+  console.log(response);
+};
