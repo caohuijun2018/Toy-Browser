b8708c42031b9870f9b4622083791ff8406f13af
caohuijun2018 <1160364324@qq.com>
2021-11-11 16:56:41 +0800

    feature():构建request和response信息

--- Server.js
+++ Server.js
@@ -1,22 +1,17 @@
-const http = require("http");
+const http = require('http');
 
-http
-  .createServer((request, response) => {
-    let body = [];
-    request
-      .on("error", (err) => {
-        console.error(err);
-      })
-      .on("data", (chunk) => {
-        body.push(chunk.toString());
-      })
-      .on("end", () => {
-        body = Buffer.concat(body).toString();
-        console.log("body:", body);
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.end("Hello World!");
-      });
-  })
-  .listen(8088);
+http.createServer((request, response) => {
+  let body = [];
+  request.on('error', (err) => {
+    console.log(err);
+  }).on('data', (chunk) => {
+    body.push(chunk.toString());
+  }).on('end', () => {
+    body = Buffer.concat(body),toString();
+    console.log('body:', body);
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(' Hello world\n');
+  });
+}).listen(8088);
 
-console.log("Server Started!");
+console.log('server started');
